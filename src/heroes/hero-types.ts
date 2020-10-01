65cfb9039f3b6ca8bcfbbebbd1417fadf709e332
Devlin D <webmasterdevlin>
2020-10-01 13:33:24 +0200

    updated

--- src/heroes/hero-types.ts
+++ src/heroes/hero-types.ts
@@ -1,25 +1,26 @@
 export type HeroStateType = {
-    heroes: Hero[],
-    hero: Hero,
-    error: string,
-    isLoading: boolean,
-  }
-  
-  export type Hero = {
-    id: string,
-    firstName: string,
-    lastName: string,
-    house: string,
-    knownAs:string
-  }
-  
-  export type HeroActionType = {
-    setHeroes: any;
-    setHero: any;
-    setError: any;
-    setIsLoading: any;
-    addHero:any;
-    removeHero:any;
-    updateHeroes:any;
-    totalHeroes: any;
-  }
+  heroes: Hero[],
+  hero: Hero,
+  error: string,
+  isLoading: boolean,
+}
+
+export type Hero = {
+  id: string,
+  firstName: string,
+  lastName: string,
+  house: string,
+  knownAs: string
+}
+
+// FIX: all any
+export type HeroActionType = {
+  setHeroes: any;
+  setHero: any;
+  setError: any;
+  setIsLoading: any;
+  addHero: any;
+  removeHero: any;
+  updateHeroes: any;
+  totalHeroes: any;
+}
