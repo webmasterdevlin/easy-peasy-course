8c0d791924d6b886a0e8edc55a1eda3d8dabfe7a
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-12-31 10:39:30 +0100

    migrated to easy peasy 4 typescript

--- src/store/combinedStores.tsx
+++ src/store/combinedStores.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StoreProvider } from "easy-peasy";
 import HeroStore from "../features/heroes/heroStore";
 
+// TODO: Add AntiHeroStore
 const CombinedStores = ({ children }: any) => {
   return <StoreProvider store={HeroStore}>{children}</StoreProvider>;
 };
